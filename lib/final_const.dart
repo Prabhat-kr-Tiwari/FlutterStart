aa36a85180f60d97a62014a347b959511f2ca00c
Prabhat Kumar Tiwari <61203736+Prabhat-kr-Tiwari@users.noreply.github.com>
2025-06-17 08:38:20 +0530

    basic of dart

---
+++ lib/final_const.dart
@@ -0,0 +1,27 @@
+main(){
+  final name =  "Prabhat";
+  // name = "heel";
+
+
+
+  final first;
+  first="hello";
+  const isReady = falase;
+  // isReady =true;
+  //const must be intialize
+  // const key;
+
+  //dynamic
+  var details;
+  details = ["hi","ne","hello"];
+  details = [12,323,23,23];
+
+
+  final test = ["Ram","shyam","john"];
+  // test =["23"];
+  test.add("new");
+
+  //const cannot be change at compile time
+
+
+}
